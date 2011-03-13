4cc7f5e58c71e9e13ac41f7bd6e5e34fb31a5ffc
Alexandre Lissy <alexandre.lissy@etu.univ-tours.fr>
2011-03-13 16:45:22 +0100

    Updating french locale
    Updating strings for french locale. Coverage should be 100%.

--- qpdfpresenterconsole_fr.ts
+++ qpdfpresenterconsole_fr.ts
@@ -2,21 +2,98 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="fr_FR">
 <context>
-    <name>QObject</name>
+    <name>QCommandLine</name>
     <message>
-        <location filename="main.cpp" line="34"/>
-        <source>File &apos;%1&apos; not found. Cannot continue.</source>
-        <translation>Le fichier &apos;%1&apos; n&apos;a pas pu ^etre chargé. Impossible de continuer.</translation>
+        <location filename="qcommandline.cpp" line="10"/>
+        <source>Display this help and exit</source>
+        <translation>Afficher cette aide et quitter</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="51"/>
+        <location filename="qcommandline.cpp" line="12"/>
+        <source>Display version and exit</source>
+        <translation>Afficher la version et quitter</translation>
+    </message>
+    <message>
+        <location filename="qcommandline.cpp" line="194"/>
+        <source>Unknown param: %1</source>
+        <translation>Paramètre « %1 » inconnu</translation>
+    </message>
+    <message>
+        <location filename="qcommandline.cpp" line="225"/>
+        <source>Unknown option: %1</source>
+        <translation>Option « %1 » inconnue</translation>
+    </message>
+    <message>
+        <location filename="qcommandline.cpp" line="240"/>
+        <location filename="qcommandline.cpp" line="249"/>
+        <source>Option %1 need a value</source>
+        <translation>L&apos;option « %1 » requiert une valeur</translation>
+    </message>
+    <message>
+        <location filename="qcommandline.cpp" line="277"/>
+        <source>Param %1 is mandatory</source>
+        <translation>Le paramètre « %1 » est nécessaire</translation>
+    </message>
+    <message>
+        <location filename="qcommandline.cpp" line="287"/>
+        <source>Switch</source>
+        <translation>Commutateur</translation>
+    </message>
+    <message>
+        <location filename="qcommandline.cpp" line="289"/>
+        <source>Option</source>
+        <translation>Option</translation>
+    </message>
+    <message>
+        <location filename="qcommandline.cpp" line="291"/>
+        <source>%1 %2 is mandatory</source>
+        <translation>%1 %2 est nécessaire</translation>
+    </message>
+    <message>
+        <location filename="qcommandline.cpp" line="465"/>
+        <source>
+Mandatory arguments to long options are mandatory for short options too.
+</source>
+        <translation>Les arguments nécessaires aux options longues le sont également pour les options courtes.</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="main.cpp" line="31"/>
+        <source>File &apos;%1&apos; not found. Cannot continue.</source>
+        <translation>Le fichier &apos;%1&apos; n&apos;a pas pu être chargé. Impossible de continuer.</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="48"/>
         <source>No multiscreen enabled. Quitting.</source>
         <translation>Pas de support multi-écran actif. Arr^et.</translation>
     </message>
     <message>
-        <location filename="mainscreenpdfview.cpp" line="80"/>
+        <location filename="mainscreenpdfview.cpp" line="101"/>
         <source>Slide %1 of %2</source>
         <translation>Diapositive %1 sur %2</translation>
+    </message>
+    <message>
+        <location filename="parameters.cpp" line="16"/>
+        <source>Duration of the presentation in minutes. Default: %1.</source>
+        <translation>Durée de la présentation en minutes. Par défaut, %1.</translation>
+    </message>
+    <message>
+        <location filename="parameters.cpp" line="21"/>
+        <source>Timer emergency in minutes. When reached, timer becomes red. Default: %1.</source>
+        <translation>Passage de la minuterie en mode urgence, en minutes. À partir de cet instant, la minuterie devient rouge. Par défaut, %1.</translation>
+    </message>
+    <message>
+        <location filename="parameters.cpp" line="26"/>
+        <source>Main slide width on computer&apos;s main screen, in percent. Default: %1.</source>
+        <translation>Largeur de la diapositive principale affichée sur l&apos;écran principal de l&apos;ordinateur, en pourcent. Par défault, %1.</translation>
+    </message>
+    <message>
+        <location filename="parameters.cpp" line="31"/>
+        <source>PDF file of the presentation.</source>
+        <oldsource>PDF file of the presentation</oldsource>
+        <translation>Fichier PDF de la présentation.</translation>
     </message>
 </context>
 </TS>
