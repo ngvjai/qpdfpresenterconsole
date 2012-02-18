c934fc9c07515f9d141e14de70c3fea7c92eede8
Alexandre Lissy <alexandre.lissy@etu.univ-tours.fr>
2012-02-18 16:15:45 +0100

    Refactor screen saver inhibition
    Using static variables, to save status.

--- screensaverinhibit.cpp
+++ screensaverinhibit.cpp
@@ -19,6 +19,16 @@ bool ScreenSaverInhibit::getCanInhibit()
     return this->canInhibit;
 }
 
+void ScreenSaverInhibit::setStatus(ScreenSaverStatus status)
+{
+    this->status = status;
+}
+
+ScreenSaverStatus ScreenSaverInhibit::getStatus()
+{
+    return this->status;
+}
+
 void ScreenSaverInhibit::allowScreenSaver()
 {
     this->setCanInhibit(true);
@@ -31,30 +41,68 @@ void ScreenSaverInhibit::dontAllowScreenSaver()
 
 void ScreenSaverInhibit::handleScreenSaverInhibition()
 {
+    ScreenSaverStatus current = this->getStatus();
+
     if (this->getCanInhibit()) {
         std::cerr << "Screen saver allowed, no inhibition." << std::endl;
         return;
     }
 
+    if (current == SCREENSAVER_INHIBITED) {
+        this->setScreenSaverDesinhibited();
+    }
+
+    if (current == SCREENSAVER_NON_INHIBITED) {
+        this->setScreenSaverInhibited();
+    }
+
+    return;
+}
+
+void ScreenSaverInhibit::setScreenSaverInhibited()
+{
+    this->switchScreenSaverInhibition(SCREENSAVER_INHIBITED);
+}
+
+void ScreenSaverInhibit::setScreenSaverDesinhibited()
+{
+    this->switchScreenSaverInhibition(SCREENSAVER_NON_INHIBITED);
+}
+
+void ScreenSaverInhibit::switchScreenSaverInhibition(ScreenSaverStatus targetStatus)
+{
 #ifdef HAVE_DBUS
-    FreedesktopInhibiter fdi;
+    static FreedesktopInhibiter fdi;
     if(fdi.canHandle()) {
-        fdi.inhibit();
-        return;
+        switch(targetStatus) {
+            case SCREENSAVER_INHIBITED:
+                fdi.inhibit();
+                break;
+        }
+        goto inhibitok;
     } else {
         std::cerr << "Freedesktop inhibiter not valid." << std::endl;
     }
 #endif
 
 #ifdef Q_OS_UNIX || Q_OS_LINUX
-    XDGInhibiter xdgi;
+    static XDGInhibiter xdgi;
     if (xdgi.canHandle()) {
-        xdgi.inhibit();
-        return;
+        switch(targetStatus) {
+            case SCREENSAVER_INHIBITED:
+                xdgi.inhibit();
+                break;
+        }
+        goto inhibitok;
     } else {
         std::cerr << "XDG inhibiter not valid." << std::endl;
     }
 #endif
 
+exit:
     return;
+
+inhibitok:
+    this->setStatus(targetStatus);
+    goto exit;
 }
