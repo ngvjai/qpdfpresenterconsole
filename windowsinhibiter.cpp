47bf0f8ab481f2c68cb82ad8dcf936addb87dbd0
Alexandre Lissy <alexandre.lissy@etu.univ-tours.fr>
2012-02-18 20:07:08 +0100

    fix: For version for Windows include
    To be able to use SetThreadExecutionState(), it is needed to redefine
    WINVER before including <windows.h>. Also, <winbase.h> is already
    included by the previous one, so no need to reinclude it.

--- windowsinhibiter.cpp
+++ windowsinhibiter.cpp
@@ -1,8 +1,14 @@
 #include "windowsinhibiter.h"
 
 #ifdef Q_WS_WIN
+/**
+ * confere include/winver.h in mingw:
+ * "If you need Win32 API features newer the Win95 and WinNT then you must
+ * define WINVER before including windows.h or any other method of including
+ * the windef.h header."
+ **/
+#define WINVER 0x0500
 #include <windows.h>
-#include <winbase.h>
 
 WindowsInhibiter::WindowsInhibiter()
 {
