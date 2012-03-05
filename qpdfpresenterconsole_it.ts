2c31dec497051ad63858c6d6403994cff99527b9
Alexandre Lissy <alexandre.lissy@etu.univ-tours.fr>
2012-03-05 11:26:42 +0100

    Italian locale attempt, thanks to pakalatak

--- qpdfpresenterconsole_it.ts
+++ qpdfpresenterconsole_it.ts
@@ -6,32 +6,33 @@
     <message>
         <location filename="mainscreenpdfview.cpp" line="34"/>
         <source>Play</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>Vedere</translatorcomment>
+        <translation>Play</translation>
     </message>
     <message>
         <location filename="mainscreenpdfview.cpp" line="35"/>
         <source>Pause</source>
-        <translation type="unfinished"></translation>
+        <translation>Pausa</translation>
     </message>
     <message>
         <location filename="mainscreenpdfview.cpp" line="36"/>
         <source>Stop</source>
-        <translation type="unfinished"></translation>
+        <translation>Fermare</translation>
     </message>
     <message>
         <location filename="mainscreenpdfview.cpp" line="39"/>
         <source>Ctrl+P</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+P</translation>
     </message>
     <message>
         <location filename="mainscreenpdfview.cpp" line="40"/>
         <source>Ctrl+A</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+A</translation>
     </message>
     <message>
         <location filename="mainscreenpdfview.cpp" line="41"/>
         <source>Ctrl+S</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+S</translation>
     </message>
 </context>
 <context>
@@ -39,64 +40,66 @@
     <message>
         <location filename="optionsdialog.ui" line="14"/>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Finestra di dialogo</translation>
     </message>
     <message>
         <location filename="optionsdialog.ui" line="25"/>
         <source>Presentation length</source>
-        <translation type="unfinished"></translation>
+        <translation>Lunghezza della presentazione</translation>
     </message>
     <message>
         <location filename="optionsdialog.ui" line="38"/>
         <location filename="optionsdialog.ui" line="58"/>
         <source>HH:mm</source>
-        <translation type="unfinished"></translation>
+        <translation>HH:mm</translation>
     </message>
     <message>
         <location filename="optionsdialog.ui" line="45"/>
         <source>Presentation emergency</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="optionsdialog.ui" line="65"/>
         <source>Slides screen prcent width</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>diapositiva ?</translatorcomment>
+        <translation>Larghezza slides percento dello schermo</translation>
     </message>
     <message>
         <location filename="optionsdialog.ui" line="86"/>
         <location filename="optionsdialog.ui" line="96"/>
         <source>0</source>
-        <translation type="unfinished"></translation>
+        <translation>0</translation>
     </message>
     <message>
         <location filename="optionsdialog.ui" line="106"/>
         <source>Main screen identifier</source>
-        <translation type="unfinished"></translation>
+        <translation>Identificatore del schemo principale</translation>
     </message>
     <message>
         <location filename="optionsdialog.ui" line="113"/>
         <source>Projector screen identifier</source>
-        <translation type="unfinished"></translation>
+        <translation>Identificatore del schermo proiettore</translation>
     </message>
     <message>
         <location filename="optionsdialog.ui" line="120"/>
         <source>Slides with Beamer Notes</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>beamer = videoproiettore</translatorcomment>
+        <translation>Diapositiva con note</translation>
     </message>
     <message>
         <location filename="optionsdialog.ui" line="130"/>
         <source>Beamer Notes on Left Screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Note su schermo di sinistra</translation>
     </message>
     <message>
         <location filename="optionsdialog.ui" line="140"/>
         <source>Beamer Notes on Right Screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Note su schermo di destra</translation>
     </message>
     <message>
         <location filename="optionsdialog.ui" line="147"/>
         <source>Use annotations in text file</source>
-        <translation type="unfinished"></translation>
+        <translation>Usa annotazione nel text file</translation>
     </message>
 </context>
 <context>
@@ -104,12 +107,12 @@
     <message>
         <location filename="pdfmodel.cpp" line="26"/>
         <source>Open PDF file</source>
-        <translation type="unfinished"></translation>
+        <translation>Apri PDF file</translation>
     </message>
     <message>
         <location filename="pdfmodel.cpp" line="26"/>
         <source>PDF Files (*.pdf)</source>
-        <translation type="unfinished"></translation>
+        <translation>PDF Files (*.pdf)</translation>
     </message>
 </context>
 <context>
@@ -117,55 +120,57 @@
     <message>
         <location filename="qcommandline.cpp" line="10"/>
         <source>Display this help and exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostra aiuto e chuidi</translation>
     </message>
     <message>
         <location filename="qcommandline.cpp" line="12"/>
         <source>Display version and exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostra versione e chuidi</translation>
     </message>
     <message>
         <location filename="qcommandline.cpp" line="194"/>
         <source>Unknown param: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Parametro sconosciuto: %1</translation>
     </message>
     <message>
         <location filename="qcommandline.cpp" line="225"/>
         <source>Unknown option: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Opzione sconosciuta: %1</translation>
     </message>
     <message>
         <location filename="qcommandline.cpp" line="240"/>
         <location filename="qcommandline.cpp" line="249"/>
         <source>Option %1 need a value</source>
-        <translation type="unfinished"></translation>
+        <translation>Impostare valore per opzione %1</translation>
     </message>
     <message>
         <location filename="qcommandline.cpp" line="277"/>
         <source>Param %1 is mandatory</source>
-        <translation type="unfinished"></translation>
+        <translation>Il parametro %1 è obbligatorio</translation>
     </message>
     <message>
         <location filename="qcommandline.cpp" line="287"/>
         <source>Switch</source>
-        <translation type="unfinished"></translation>
+        <translation>Cambiare</translation>
     </message>
     <message>
         <location filename="qcommandline.cpp" line="289"/>
         <source>Option</source>
-        <translation type="unfinished"></translation>
+        <translation>Opzione</translation>
     </message>
     <message>
         <location filename="qcommandline.cpp" line="291"/>
         <source>%1 %2 is mandatory</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 %2 sono obbligatori</translation>
     </message>
     <message>
         <location filename="qcommandline.cpp" line="465"/>
         <source>
 Mandatory arguments to long options are mandatory for short options too.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+Valore obbligatorie per lunghe opzione sono anche obbligatorie per opzione corte.
+</translation>
     </message>
 </context>
 <context>
@@ -173,97 +178,97 @@ Mandatory arguments to long options are mandatory for short options too.
     <message>
         <location filename="main.cpp" line="33"/>
         <source>No multiscreen enabled. Quitting.</source>
-        <translation type="unfinished"></translation>
+        <translation>Multi schermi non autorizati. Uscita.</translation>
     </message>
     <message>
         <location filename="main.cpp" line="44"/>
         <source>File &apos;%1&apos; not found. Cannot continue.</source>
-        <translation type="unfinished"></translation>
+        <translation>File &apos;%1&apos; non trovato. Non è possibile proseguire.</translation>
     </message>
     <message>
         <location filename="mainscreenpdfview.cpp" line="173"/>
         <source>Going to a specific page number</source>
-        <translation type="unfinished"></translation>
+        <translation>Andando verso un specificao numero di pagina</translation>
     </message>
     <message>
         <location filename="mainscreenpdfview.cpp" line="174"/>
         <source>Going to page:</source>
-        <translation type="unfinished"></translation>
+        <translation>Andando verso la pagina:</translation>
     </message>
     <message>
         <location filename="mainscreenpdfview.cpp" line="227"/>
         <source>Slide %1 of %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Diapositiva %1 di %2</translation>
     </message>
     <message>
         <location filename="optionsdialog.cpp" line="20"/>
         <source>Parameters management</source>
-        <translation type="unfinished"></translation>
+        <translation>Cambiare parametri</translation>
     </message>
     <message>
         <location filename="parameters.cpp" line="17"/>
         <source>Duration of the presentation in minutes. Default: %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Presentazione durata in minuti. default: %1.</translation>
     </message>
     <message>
         <location filename="parameters.cpp" line="22"/>
         <source>Timer emergency in minutes. When reached, timer becomes red. Default: %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Timer emergenza in minuti. Quando raggiunto, il timer diventà rosso. Default: %1.</translation>
     </message>
     <message>
         <location filename="parameters.cpp" line="27"/>
         <source>Main slide width on computer&apos;s main screen, in percent. Default: %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Larghezza della diapositiva principale sul schermo dell computer, in percento. Default: %1.</translation>
     </message>
     <message>
         <location filename="parameters.cpp" line="32"/>
         <source>Starting page number. PDF will be opened at this page. Default: %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Numero della prima pagina. PDF sara aperto a questo punto. Default: %1.</translation>
     </message>
     <message>
         <location filename="parameters.cpp" line="37"/>
         <source>Main screen identifier. This is where augmented presentation will be. Default: %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Identificatore del schermo principale. La presentazione augmentata sara qui. Default: %1.</translation>
     </message>
     <message>
         <location filename="parameters.cpp" line="42"/>
         <source>Projector screen identifier. This is where classical presentation will be. Default: %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Identificatore del schermo proiettatore. La presentazione classica sara qui. Default: %1.</translation>
     </message>
     <message>
         <location filename="parameters.cpp" line="47"/>
         <source>Enable support for Beamer notes. Default: %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Autorizza assitenza per les note proiettore. Default: %1.</translation>
     </message>
     <message>
         <location filename="parameters.cpp" line="52"/>
         <source>Which part of the slides for Beamer notes (left or right screen). Default: %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Lato diapositiva per le note proiettore (sinistra oppure destra del schermo). Default: %1.</translation>
     </message>
     <message>
         <location filename="parameters.cpp" line="57"/>
         <source>Use annotations from text file. Default: %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Usa annotazione del text file. Default: %1.</translation>
     </message>
     <message>
         <location filename="parameters.cpp" line="62"/>
         <source>Check the presence of multiple screen. Default: %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Verifica la presenza di multi schermi. Default: %1.</translation>
     </message>
     <message>
         <location filename="parameters.cpp" line="67"/>
         <source>Config file to load parameters from. Any other command line parameter will be ignored. Default: %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Config. file per scarricare parametri. Altri parametrei di linea commandi saranno trascurati. Default: %1.</translation>
     </message>
     <message>
         <location filename="parameters.cpp" line="72"/>
         <source>PDF file of the presentation.</source>
-        <translation type="unfinished"></translation>
+        <translation>PDF file della presentazione.</translation>
     </message>
     <message>
         <location filename="pdfmodel.cpp" line="87"/>
         <source>Text file annotations for pdf &apos;%1&apos; not found. Cannot use text file annotations, disabling for now. File(s) expected: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Text file annotazione per il PDF &apos;%1&apos; non trovato. Non è possibilie usare file annotazione, sono disattivate per il momento. File(s) aspettati: %2</translation>
     </message>
 </context>
 </TS>
