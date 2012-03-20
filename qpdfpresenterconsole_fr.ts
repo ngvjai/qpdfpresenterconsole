f576c7faa02bdc7c4d685707a7d119e6c27ee844
Alexandre Lissy <alexandre.lissy@etu.univ-tours.fr>
2012-03-20 12:41:42 +0100

    Updated french localization

--- qpdfpresenterconsole_fr.ts
+++ qpdfpresenterconsole_fr.ts
@@ -4,34 +4,104 @@
 <context>
     <name>MainScreenPdfView</name>
     <message>
-        <location filename="mainscreenpdfview.cpp" line="34"/>
+        <location filename="mainscreenpdfview.cpp" line="39"/>
+        <source>Set media position</source>
+        <translation>Définir la position dans le média</translation>
+    </message>
+    <message>
+        <location filename="mainscreenpdfview.cpp" line="41"/>
+        <source>Set media volume</source>
+        <translation>Définir le volume du média</translation>
+    </message>
+    <message>
+        <location filename="mainscreenpdfview.cpp" line="44"/>
+        <source>Backward</source>
+        <translation>En arrière</translation>
+    </message>
+    <message>
+        <location filename="mainscreenpdfview.cpp" line="45"/>
+        <source>Forward</source>
+        <translation>En avant</translation>
+    </message>
+    <message>
+        <location filename="mainscreenpdfview.cpp" line="46"/>
         <source>Play</source>
         <translation>Jouer</translation>
     </message>
     <message>
-        <location filename="mainscreenpdfview.cpp" line="35"/>
+        <location filename="mainscreenpdfview.cpp" line="47"/>
         <source>Pause</source>
         <translation>Pause</translation>
     </message>
     <message>
-        <location filename="mainscreenpdfview.cpp" line="36"/>
+        <location filename="mainscreenpdfview.cpp" line="48"/>
         <source>Stop</source>
         <translation>Arrêter</translation>
     </message>
     <message>
-        <location filename="mainscreenpdfview.cpp" line="39"/>
+        <location filename="mainscreenpdfview.cpp" line="49"/>
         <source>Ctrl+P</source>
         <translation>Ctrl+P</translation>
     </message>
     <message>
-        <location filename="mainscreenpdfview.cpp" line="40"/>
+        <location filename="mainscreenpdfview.cpp" line="50"/>
         <source>Ctrl+A</source>
         <translation>Ctrl+A</translation>
     </message>
     <message>
-        <location filename="mainscreenpdfview.cpp" line="41"/>
+        <location filename="mainscreenpdfview.cpp" line="51"/>
         <source>Ctrl+S</source>
         <translation>Ctrl+S</translation>
+    </message>
+    <message>
+        <location filename="mainscreenpdfview.cpp" line="52"/>
+        <source>Ctrl+N</source>
+        <translation>Ctrl+N</translation>
+    </message>
+    <message>
+        <location filename="mainscreenpdfview.cpp" line="53"/>
+        <source>Ctrl+R</source>
+        <translation>Ctrl+R</translation>
+    </message>
+    <message>
+        <location filename="mainscreenpdfview.cpp" line="64"/>
+        <source>Volume</source>
+        <translation>Volume</translation>
+    </message>
+    <message>
+        <location filename="mainscreenpdfview.cpp" line="69"/>
+        <source>Previous slide</source>
+        <translation>Diapositive précédente</translation>
+    </message>
+    <message>
+        <location filename="mainscreenpdfview.cpp" line="73"/>
+        <source>Next slide</source>
+        <translation>Diapositive suivante</translation>
+    </message>
+    <message>
+        <location filename="mainscreenpdfview.cpp" line="77"/>
+        <source>Options</source>
+        <translation>Options</translation>
+    </message>
+    <message>
+        <location filename="mainscreenpdfview.cpp" line="81"/>
+        <source>Help</source>
+        <translation>Aide</translation>
+    </message>
+    <message>
+        <location filename="mainscreenpdfview.cpp" line="85"/>
+        <source>Goto slide</source>
+        <translation>Aller à la diapositive</translation>
+    </message>
+    <message>
+        <location filename="mainscreenpdfview.cpp" line="89"/>
+        <source>Presentation/Desktop Mode</source>
+        <translation>Mode présentation/bureau</translation>
+    </message>
+    <message>
+        <location filename="mainscreenpdfview.cpp" line="93"/>
+        <source>Quit</source>
+        <translation>Quitter</translation>
     </message>
 </context>
 <context>
@@ -102,12 +172,12 @@
 <context>
     <name>PDFModel</name>
     <message>
-        <location filename="pdfmodel.cpp" line="26"/>
+        <location filename="pdfmodel.cpp" line="28"/>
         <source>Open PDF file</source>
         <translation>Ouvrir le fichier PDF</translation>
     </message>
     <message>
-        <location filename="pdfmodel.cpp" line="26"/>
+        <location filename="pdfmodel.cpp" line="28"/>
         <source>PDF Files (*.pdf)</source>
         <translation>Fichiers PDF (*.pdf)</translation>
     </message>
@@ -115,53 +185,53 @@
 <context>
     <name>QCommandLine</name>
     <message>
-        <location filename="qcommandline.cpp" line="10"/>
+        <location filename="qcommandline.cpp" line="12"/>
         <source>Display this help and exit</source>
         <translation>Afficher cette aide et quitter</translation>
     </message>
     <message>
-        <location filename="qcommandline.cpp" line="12"/>
+        <location filename="qcommandline.cpp" line="14"/>
         <source>Display version and exit</source>
         <translation>Afficher la version et quitter</translation>
     </message>
     <message>
-        <location filename="qcommandline.cpp" line="194"/>
+        <location filename="qcommandline.cpp" line="196"/>
         <source>Unknown param: %1</source>
         <translation>Paramètre « %1 » inconnu</translation>
     </message>
     <message>
-        <location filename="qcommandline.cpp" line="225"/>
+        <location filename="qcommandline.cpp" line="227"/>
         <source>Unknown option: %1</source>
         <translation>Option « %1 » inconnue</translation>
     </message>
     <message>
-        <location filename="qcommandline.cpp" line="240"/>
-        <location filename="qcommandline.cpp" line="249"/>
+        <location filename="qcommandline.cpp" line="242"/>
+        <location filename="qcommandline.cpp" line="251"/>
         <source>Option %1 need a value</source>
         <translation>L&apos;option « %1 » requiert une valeur</translation>
     </message>
     <message>
-        <location filename="qcommandline.cpp" line="277"/>
+        <location filename="qcommandline.cpp" line="279"/>
         <source>Param %1 is mandatory</source>
         <translation>Le paramètre « %1 » est nécessaire</translation>
     </message>
     <message>
-        <location filename="qcommandline.cpp" line="287"/>
+        <location filename="qcommandline.cpp" line="289"/>
         <source>Switch</source>
         <translation>Commutateur</translation>
     </message>
     <message>
-        <location filename="qcommandline.cpp" line="289"/>
+        <location filename="qcommandline.cpp" line="291"/>
         <source>Option</source>
         <translation>Option</translation>
     </message>
     <message>
-        <location filename="qcommandline.cpp" line="291"/>
+        <location filename="qcommandline.cpp" line="293"/>
         <source>%1 %2 is mandatory</source>
         <translation>%1 %2 est nécessaire</translation>
     </message>
     <message>
-        <location filename="qcommandline.cpp" line="465"/>
+        <location filename="qcommandline.cpp" line="467"/>
         <source>
 Mandatory arguments to long options are mandatory for short options too.
 </source>
@@ -173,100 +243,120 @@ Les arguments nécessaires aux options longues le sont également pour les optio
 <context>
     <name>QObject</name>
     <message>
-        <location filename="main.cpp" line="44"/>
+        <location filename="main.cpp" line="46"/>
         <source>File &apos;%1&apos; not found. Cannot continue.</source>
         <translation>Le fichier &apos;%1&apos; n&apos;a pas pu être chargé. Impossible de continuer.</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="33"/>
+        <location filename="main.cpp" line="35"/>
         <source>No multiscreen enabled. Quitting.</source>
         <translation>Pas de support multi-écran actif. Arrêt.</translation>
     </message>
     <message>
-        <location filename="mainscreenpdfview.cpp" line="173"/>
+        <location filename="mainscreenpdfview.cpp" line="252"/>
         <source>Going to a specific page number</source>
         <translation>Aller à une page précise</translation>
     </message>
     <message>
-        <location filename="mainscreenpdfview.cpp" line="174"/>
+        <location filename="mainscreenpdfview.cpp" line="253"/>
         <source>Going to page:</source>
         <translation>Aller à la page :</translation>
     </message>
     <message>
-        <location filename="mainscreenpdfview.cpp" line="227"/>
+        <location filename="mainscreenpdfview.cpp" line="336"/>
         <source>Slide %1 of %2</source>
         <translation>Diapositive %1 sur %2</translation>
     </message>
     <message>
-        <location filename="parameters.cpp" line="17"/>
+        <location filename="mainscreenpdfview.cpp" line="342"/>
+        <source>Next slide</source>
+        <translation>Diapositive suivante</translation>
+    </message>
+    <message>
+        <location filename="mainscreenpdfview.cpp" line="344"/>
+        <source>Current slide</source>
+        <translation>Diapositive en cours</translation>
+    </message>
+    <message>
+        <location filename="parameters.cpp" line="19"/>
         <source>Duration of the presentation in minutes. Default: %1.</source>
         <translation>Durée de la présentation en minutes. Par défaut, %1.</translation>
     </message>
     <message>
-        <location filename="parameters.cpp" line="22"/>
+        <location filename="parameters.cpp" line="24"/>
         <source>Timer emergency in minutes. When reached, timer becomes red. Default: %1.</source>
         <translation>Passage de la minuterie en mode urgence, en minutes. À partir de cet instant, la minuterie devient rouge. Par défaut, %1.</translation>
     </message>
     <message>
-        <location filename="parameters.cpp" line="27"/>
+        <location filename="parameters.cpp" line="29"/>
         <source>Main slide width on computer&apos;s main screen, in percent. Default: %1.</source>
         <translation>Largeur de la diapositive principale affichée sur l&apos;écran principal de l&apos;ordinateur, en pourcent. Par défault, %1.</translation>
     </message>
     <message>
-        <location filename="parameters.cpp" line="32"/>
+        <location filename="parameters.cpp" line="34"/>
         <source>Starting page number. PDF will be opened at this page. Default: %1.</source>
         <translation>Numéro de la page de départ. Le fichier PDF sera ouvert à cette page .Par défaut, %1.</translation>
     </message>
     <message>
-        <location filename="parameters.cpp" line="37"/>
+        <location filename="parameters.cpp" line="39"/>
         <source>Main screen identifier. This is where augmented presentation will be. Default: %1.</source>
         <translation>Identifiant de l&apos;écran principal. C&apos;est sur cet écran que la présentation agrémentée d&apos;informations supplémentaire sera affichée. Par défaut, %1.</translation>
     </message>
     <message>
-        <location filename="parameters.cpp" line="42"/>
+        <location filename="parameters.cpp" line="44"/>
         <source>Projector screen identifier. This is where classical presentation will be. Default: %1.</source>
         <translation>Identifiant du projecteur. C&apos;est sur cet écran que la présentation « classique » sera affichée. Par défaut, %1.</translation>
     </message>
     <message>
-        <location filename="parameters.cpp" line="47"/>
+        <location filename="parameters.cpp" line="49"/>
         <source>Enable support for Beamer notes. Default: %1.</source>
         <translation>Activer la prise en charge des notes Beamer. Par défaut, %1.</translation>
     </message>
     <message>
-        <location filename="parameters.cpp" line="52"/>
+        <location filename="parameters.cpp" line="54"/>
         <source>Which part of the slides for Beamer notes (left or right screen). Default: %1.</source>
         <translation>Quelle partie des diapositives contient les notes Beamer (partie droite &apos;right&apos; ou gauche &apos;left&apos;). Par défaut, %1.</translation>
     </message>
     <message>
-        <location filename="parameters.cpp" line="57"/>
+        <location filename="parameters.cpp" line="59"/>
         <source>Use annotations from text file. Default: %1.</source>
         <translation>Utiliser les annotations contenues dans un fichier texte. Par défaut, %1.</translation>
     </message>
     <message>
-        <location filename="parameters.cpp" line="62"/>
+        <location filename="parameters.cpp" line="64"/>
         <source>Check the presence of multiple screen. Default: %1.</source>
         <translation>Vérifier la présence de plusieurs écrans. Par défaut, %1.</translation>
     </message>
     <message>
-        <location filename="parameters.cpp" line="67"/>
+        <location filename="parameters.cpp" line="69"/>
         <source>Config file to load parameters from. Any other command line parameter will be ignored. Default: %1.</source>
         <translation>Fichier de configuration à utiliser pour charger les paramètres. Tous les autres paramètres passés seront ignorés. Par défault, %1.</translation>
     </message>
     <message>
-        <location filename="parameters.cpp" line="72"/>
+        <location filename="parameters.cpp" line="74"/>
         <source>PDF file of the presentation.</source>
         <oldsource>PDF file of the presentation</oldsource>
         <translation>Fichier PDF de la présentation.</translation>
     </message>
     <message>
-        <location filename="optionsdialog.cpp" line="20"/>
+        <location filename="optionsdialog.cpp" line="22"/>
         <source>Parameters management</source>
         <translation>Gestion des paramètres</translation>
     </message>
     <message>
-        <location filename="pdfmodel.cpp" line="87"/>
+        <location filename="pdfmodel.cpp" line="118"/>
         <source>Text file annotations for pdf &apos;%1&apos; not found. Cannot use text file annotations, disabling for now. File(s) expected: %2</source>
         <translation>Annotations contenues dans un fichier texte introuvable pour le document PDF &apos;%1&apos;. Impossible de les utiliser, désactivation pour le moment. Fichier(s) attendu(s) : %2</translation>
+    </message>
+    <message>
+        <location filename="mediaplayer.cpp" line="123"/>
+        <source>Cannot redirect standard error output to file &apos;%1&apos;.</source>
+        <translation>Impossible de rediriger la sortie d&apos;erreur standard vers le fichier &apos;%1&apos;.</translation>
+    </message>
+    <message>
+        <location filename="mediaplayer.cpp" line="126"/>
+        <source>Standard error has been redirected to &apos;%1&apos;.</source>
+        <translation>La sortie d&apos;erreur standard a été redirigée vers le fichier &apos;%1&apos;.</translation>
     </message>
 </context>
 </TS>
